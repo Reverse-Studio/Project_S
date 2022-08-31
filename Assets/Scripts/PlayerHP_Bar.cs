7d60ade9fbbcbdb643b439e6f67d6f8af1e879c7
roopha <80938105+roopgha@users.noreply.github.com>
2022-08-31 10:25:48 +0900

    add player Hp_Bar

---
+++ Assets/Scripts/PlayerHP_Bar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP_Bar : MonoBehaviour
+{
+    public Transform player;
+    public Slider hpbar;
+    public float maxHp;
+    public float currenthp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + new Vector3(0, 0, 0);
+        hpbar.value = currenthp / maxHp;
+    }
+}
