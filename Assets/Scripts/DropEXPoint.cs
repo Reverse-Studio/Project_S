9a346fa9716c9639d8b3e96eaba2e049b5c472a7
rhdwn22c <rhdwn22c@gmail.com>
2022-09-03 19:50:01 +0900

    exp

---
+++ Assets/Scripts/DropEXPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leveling : MonoBehaviour
+{
+    public GameObject player;
+    private int level = 0;
+    private int levelPoint = 0;
+
+    private void Start()
+    {
+        level = 0;
+        levelPoint = 0;
+    }
+
+    private void Update()
+    {
+
+    }
+}
