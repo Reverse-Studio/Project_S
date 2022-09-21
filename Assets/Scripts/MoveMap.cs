8fbe1bc6ce55b4e363c41221f86b856fbe1e8f98
rhdwn22c <rhdwn22c@gmail.com>
2022-09-21 18:12:37 +0900

    update mapmove

--- Assets/Scripts/MoveMap.cs
+++ Assets/Scripts/MoveMap.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapSpawner : MonoBehaviour
+public class MoveMap : MonoBehaviour
 {
     public GameObject[] mapPrefab; 
     public GameObject player;
