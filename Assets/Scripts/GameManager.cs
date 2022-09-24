dac85e7b71b46c162fb6a19216273f34970f1ffa
roopgha <z01073837705@gmail.com>
2022-09-24 16:52:13 +0900

    Add Pause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager __instance__ = null;
     public static GameManager INSTANCE { get => __instance__; }
-
     private GameObject orb;
+    bool isPause = false;
+    [SerializeField]
+    private GameObject popup;
+    private Button pause;
+    private Button background;
+    private Button resume;
 
     private void Awake()
     {
         orb = Resources.Load<GameObject>("Prefab/ExpOrb");
+        popup.SetActive(false);
         DontDestroyOnLoad(gameObject);
         __instance__ = this;
     }
@@ -28,10 +36,33 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+     
+    }
+
     public void SpawnExpOrb(Vector3 location, int amount)
     {
         GameObject orbInst = Instantiate(orb, location, Quaternion.identity);
         ExpOrb expOrb = orbInst.GetComponent<ExpOrb>();
         expOrb.Amount = amount;
+    }
+
+    public void SetPause()
+    {
+        if(isPause == false)
+        {
+            Time.timeScale = 0f;
+            isPause = true;
+        }
+    }
+
+    public void SetStart()
+    {
+        if(isPause == true)
+        {
+            Time.timeScale = 1f;
+            isPause = false;
+        }
     }
 }
