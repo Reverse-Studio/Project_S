b26604b005fabcceed99e366dbebcf045ee04b6c
rhdwn22c <rhdwn22c@gmail.com>
2022-09-26 19:53:33 +0900

    Size up Map

--- Assets/Scripts/MoveMap.cs
+++ Assets/Scripts/MoveMap.cs
@@ -61,9 +61,9 @@ public class MoveMap : MonoBehaviour
         Swap(mapPrefab, 0, 3, 6);
         Swap(mapPrefab, 1, 4, 7);
         Swap(mapPrefab, 2, 5, 8);
-        mapPrefab[6].gameObject.transform.position += new Vector3(0, 0, -60);
-        mapPrefab[7].gameObject.transform.position += new Vector3(0, 0, -60);
-        mapPrefab[8].gameObject.transform.position += new Vector3(0, 0, -60);
+        mapPrefab[6].gameObject.transform.position += new Vector3(0, 0, -90);
+        mapPrefab[7].gameObject.transform.position += new Vector3(0, 0, -90);
+        mapPrefab[8].gameObject.transform.position += new Vector3(0, 0, -90);
         posZ = player.gameObject.transform.position.z;
     }
 
@@ -71,9 +71,9 @@ public class MoveMap : MonoBehaviour
         Swap(mapPrefab, 6, 3, 0);
         Swap(mapPrefab, 7, 4, 1);
         Swap(mapPrefab, 8, 5, 2);
-        mapPrefab[0].gameObject.transform.position += new Vector3(0, 0, 60);
-        mapPrefab[1].gameObject.transform.position += new Vector3(0, 0, 60);
-        mapPrefab[2].gameObject.transform.position += new Vector3(0, 0, 60);
+        mapPrefab[0].gameObject.transform.position += new Vector3(0, 0, 90);
+        mapPrefab[1].gameObject.transform.position += new Vector3(0, 0, 90);
+        mapPrefab[2].gameObject.transform.position += new Vector3(0, 0, 90);
         posZ = player.gameObject.transform.position.z;
     }
 
@@ -81,9 +81,9 @@ public class MoveMap : MonoBehaviour
         Swap(mapPrefab, 0, 1, 2);
         Swap(mapPrefab, 3, 4, 5);
         Swap(mapPrefab, 6, 7, 8);
-        mapPrefab[2].gameObject.transform.position += new Vector3(60, 0, 0);
-        mapPrefab[5].gameObject.transform.position += new Vector3(60, 0, 0);
-        mapPrefab[8].gameObject.transform.position += new Vector3(60, 0, 0);
+        mapPrefab[2].gameObject.transform.position += new Vector3(90, 0, 0);
+        mapPrefab[5].gameObject.transform.position += new Vector3(90, 0, 0);
+        mapPrefab[8].gameObject.transform.position += new Vector3(90, 0, 0);
         posX = player.gameObject.transform.position.x;
     }
 
@@ -91,9 +91,9 @@ public class MoveMap : MonoBehaviour
         Swap(mapPrefab, 2, 1, 0);
         Swap(mapPrefab, 5, 4, 3);
         Swap(mapPrefab, 8, 7, 6);
-        mapPrefab[0].gameObject.transform.position += new Vector3(-60, 0, 0);
-        mapPrefab[3].gameObject.transform.position += new Vector3(-60, 0, 0);
-        mapPrefab[6].gameObject.transform.position += new Vector3(-60, 0, 0);
+        mapPrefab[0].gameObject.transform.position += new Vector3(-90, 0, 0);
+        mapPrefab[3].gameObject.transform.position += new Vector3(-90, 0, 0);
+        mapPrefab[6].gameObject.transform.position += new Vector3(-90, 0, 0);
         posX = player.gameObject.transform.position.x;
     }
 
