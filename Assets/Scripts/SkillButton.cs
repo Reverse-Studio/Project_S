31073e6b6b129625f0fd0a4d351d058ac5b2c877
changwook987 <changwook987@naver.com>
2022-10-20 17:45:39 +0900

    Changed Script Directory

--- Assets/Scripts/SkillButton.cs
+++ Assets/Scripts/SkillButton.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharSpin : MonoBehaviour
+public class SkillButton : MonoBehaviour
 {
-    public float StandardSpeed = 100f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,6 @@ public class CharSpin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(StandardSpeed * Time.deltaTime, StandardSpeed * Time.deltaTime, StandardSpeed * Time.deltaTime));    
+        
     }
 }
